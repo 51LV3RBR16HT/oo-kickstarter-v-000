90bf941a78ee1de7f4ad44202a260e0cd03fce91
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-22 18:43:13 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,9 +6,9 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(Project.new)
-    project = Project.new
-    @backed_projects << project 
+  def back_project(Project)
+    x = Project.new
+    @backed_projects << x
   end
 
 end
